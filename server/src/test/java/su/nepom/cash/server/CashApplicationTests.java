e1d0492968be984016d27447acd9f80bd9706990
evgnep <enji81@gmail.com>
2020-05-26 20:16:50 +0300

    Заготовка для сервера

---
+++ server/src/test/java/su/nepom/cash/server/CashApplicationTests.java
@@ -0,0 +1,15 @@
+package su.nepom.cash.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
