7e73837cc14146e78b63face18076a04e745bc58
evgnep <enji81@gmail.com>
2020-07-27 21:03:32 +0300

    Доработки по результатам ревью #16

--- server/src/main/java/su/nepom/cash/server/remote/security/SecurityConfig.java
+++ server/src/main/java/su/nepom/cash/server/remote/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui/**", "/swagger/**");
+        web.ignoring().antMatchers("/swagger-ui/**", "/swagger/**", "/v3/api-docs/**");
     }
 
     @Override
@@ -62,7 +62,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         //- проводки
                         .antMatchers("/api/record/**").hasAnyRole(CHILD, PARENT) // доп проверки на уровне методов
                         //--
-                        .antMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().denyAll()
                 );
     }
